5bea07d13968aa6413f588727cb0101848769a82
Dan Slattery <dan@Dans-MacBook-Air.local>
2024-02-24 14:57:00 +1100

    removing nonchoc

--- keyboards/sofle/sofle.h
+++ keyboards/sofle/sofle.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#if defined(KEYBOARD_sofle_rev1)
-#    include "rev1.h"
-#elif defined(KEYBOARD_sofle_keyhive)
-#    include "keyhive.h"
-#elif defined(KEYBOARD_sofle_choc)
+#if defined(KEYBOARD_sofle_choc)
 #    include "choc.c"
 #endif
